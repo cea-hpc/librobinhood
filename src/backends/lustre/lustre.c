6066ba8b454f8cf7fb548e37dac47adfd61d3cbb
Yoann Valeri <yoann.valeri@cea.fr>
2022-04-28 09:34:31 +0200

    lustre: record basic "layout" attributes as xattrs
    Change-Id: I49ac7fb3282698420889eff1ebdd9a3bcd804cef
    Signed-off-by: Yoann Valeri <yoann.valeri@cea.fr>

--- src/backends/lustre/lustre.c
+++ src/backends/lustre/lustre.c
@@ -2,6 +2,10 @@
 # include "config.h"
 #endif
 
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/xattr.h>
+
 #include <lustre/lustreapi.h>
 
 #include "robinhood/backends/posix.h"
@@ -86,6 +90,82 @@ xattrs_get_hsm(int fd, struct rbh_value_pair *pairs)
     return subcount;
 }
 
+static int
+xattrs_get_magic_and_gen(int fd, struct rbh_value_pair *pairs)
+{
+    char lov_buf[XATTR_SIZE_MAX];
+    ssize_t xattr_size;
+    uint32_t magic = 0;
+    int subcount = 0;
+    uint32_t gen = 0;
+    int rc;
+
+    xattr_size = fgetxattr(fd, XATTR_LUSTRE_LOV, lov_buf, sizeof(lov_buf));
+    if (xattr_size < 0)
+        return -1;
+
+    magic = ((struct lov_user_md *) lov_buf)->lmm_magic;
+
+    switch (magic) {
+    case LOV_USER_MAGIC_V1:
+        gen = ((struct lov_user_md_v1 *) lov_buf)->lmm_layout_gen;
+        break;
+    case LOV_USER_MAGIC_V3:
+        gen = ((struct lov_user_md_v3 *) lov_buf)->lmm_layout_gen;
+        break;
+    case LOV_USER_MAGIC_COMP_V1:
+        gen = ((struct lov_comp_md_v1 *) lov_buf)->lcm_layout_gen;
+        break;
+    default:
+        break;
+    }
+
+    rc = fill_uint32_pair(magic, &pairs[subcount++], "magic");
+    if (rc)
+        return -1;
+
+    rc = fill_uint32_pair(gen, &pairs[subcount++], "gen");
+    if (rc)
+        return -1;
+
+    return subcount;
+}
+
+static int
+xattrs_get_layout(int fd, struct rbh_value_pair *pairs)
+{
+    struct llapi_layout *layout;
+    int subcount = 0;
+    uint32_t flags;
+    int rc;
+
+    layout = llapi_layout_get_by_fd(fd, 0);
+    if (layout == NULL)
+        return -1;
+
+    rc = llapi_layout_flags_get(layout, &flags);
+    if (rc)
+        goto err;
+
+    rc = fill_uint32_pair(flags, &pairs[subcount++], "flags");
+    if (rc)
+        goto err;
+
+    if (!is_dir) {
+        rc = xattrs_get_magic_and_gen(fd, &pairs[subcount]);
+        if (rc < 0)
+            goto err;
+        else
+            subcount += rc;
+
+        rc = 0;
+    }
+
+err:
+    llapi_layout_free(layout);
+    return rc ? rc : subcount;
+}
+
 static ssize_t
 lustre_ns_xattrs_callback(int fd, struct rbh_value_pair *pairs,
                           size_t *_ns_pairs_count, struct rbh_sstack *values)
@@ -115,6 +195,12 @@ lustre_ns_xattrs_callback(int fd, struct rbh_value_pair *pairs,
 
         count += subcount;
     }
+
+    subcount = xattrs_get_layout(fd, &pairs[count]);
+    if (subcount == -1)
+        return -1;
+
+    count += subcount;
 
     return count;
 }
