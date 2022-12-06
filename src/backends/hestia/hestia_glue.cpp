b12f0b92358c0dbf19b2bbe88ac6235b62998db6
VALERI Yoann <yoann.valeri@cea.fr>
2022-12-06 13:34:07 +0100

    hestia: retrieve all objects of all tiers
    This patchs implements the retrieval of all objects
    accross all tiers in a single array. Using this
    array, the iterator will be able to retrieve all
    object attributes one at a time.
    
    Change-Id: I84a09e61e53c3fe538de3e2b730f0c798fbc8ccd
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- src/backends/hestia/hestia_glue.cpp
+++ src/backends/hestia/hestia_glue.cpp
@@ -15,3 +15,37 @@ int list_tiers(uint8_t **tiers, size_t *len)
     std::copy(hestia_tiers.begin(), hestia_tiers.end(), *tiers);
     return 0;
 }
+
+int list_objects(uint8_t *tiers, size_t tiers_len,
+                 struct hestia_id **ids, size_t *ids_len)
+{
+    *ids_len = 0;
+
+    for (size_t i = 0; i < tiers_len; ++i) {
+        std::vector<struct hestia::hsm_uint> hestia_objects =
+            hestia::list(tiers[i]);
+        void *tmp;
+
+        if (hestia_objects.size() == 0)
+            continue;
+
+        if (*ids == NULL)
+            tmp = malloc(sizeof(**ids) * hestia_objects.size());
+        else
+            tmp = realloc(*ids,
+                          sizeof(**ids) * (*ids_len + hestia_objects.size()));
+
+        if (tmp == NULL)
+            return -1;
+
+        *ids = (struct hestia_id *) tmp;
+
+        for (size_t j = 0; j < hestia_objects.size(); ++j) {
+            (*ids)[j + *ids_len].higher = hestia_objects[j].higher;
+            (*ids)[j + *ids_len].lower = hestia_objects[j].lower;
+        }
+        *ids_len += hestia_objects.size();
+    }
+
+    return 0;
+}
--- src/backends/hestia/hestia_glue.h
+++ src/backends/hestia/hestia_glue.h
@@ -5,7 +5,14 @@
 extern "C" {
 #endif
 
+struct hestia_id {
+    uint64_t higher;
+    uint64_t lower;
+};
+
 int list_tiers(uint8_t **tiers, size_t *len);
+int list_objects(uint8_t *tiers, size_t tiers_len,
+                 struct hestia_id **ids, size_t *ids_len);
 
 #ifdef __cplusplus
 }
